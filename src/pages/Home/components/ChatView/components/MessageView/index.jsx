390a8c4d32447c2f371b4410a75a8fc3ec544cf8
penry28 <phamdinhhung28.it@gmail.com>
2022-08-02 22:39:16 +0700

    [Coding] Layout

--- src/pages/Home/components/ChatView/components/MessageView/index.jsx
+++ src/pages/Home/components/ChatView/components/MessageView/index.jsx
@@ -2,7 +2,7 @@ function MessageView() {
     return (
         <div className="message-view">
             <div className="message-view__blur__overlay"></div>
-            <div className="message-View-container">
+            <div className="message-view-container">
                 <div className="message-view__scroll__inner"></div>
             </div>
         </div>
